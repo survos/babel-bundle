e628cc58aa60adb4d79ea6526c03383fbf66fa05
tacman <tacman@users.noreply.github.com>
2026-05-06 21:15:09 +0000

    add command-schema

--- src/Attribute/BabelStorage.php
+++ src/Attribute/BabelStorage.php
@@ -3,11 +3,6 @@ declare(strict_types=1);
 
 namespace Survos\BabelBundle\Attribute;
 
-enum StorageMode: string {
-    case Code = 'code'; // embedded in $tCode, used by Pixie
-    case Property = 'property'; // a single string property, e.g. $label_backed.
-}
-
 #[\Attribute(\Attribute::TARGET_CLASS)]
 final class BabelStorage
 {
